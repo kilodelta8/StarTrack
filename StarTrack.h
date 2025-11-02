28e99a91465bfea9b3c2bc7959d2a88ed43ac52f
kilodelta8 <jdurham937@gmail.com>
2025-11-02 03:54:30 -0500

    Started STar Track Lib

---
+++ StarTrack.h
@@ -0,0 +1,15 @@
+#ifndef STARTRACK_H
+#define STARTRACK_H
+
+class StarTrack {
+    private:
+
+
+    public:
+        StarTrack();
+        ~StarTrack();
+        void initialize();
+        void update();
+        void render();};
+
+#endif // STARTRACK_H
